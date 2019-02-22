3fd72cd56339922226f0bc23b4e6a1b889e8d52f
unknown <eriznis@gmail.com>
2019-02-22 17:18:15 -0500

    all the tabs are changing, gotta answer the questions and do the proptypes

--- lambdatimes/src/components/Content/Content.js
+++ lambdatimes/src/components/Content/Content.js
@@ -23,7 +23,12 @@ export default class Content extends Component {
   }
 
   changeSelected = tab => {
+    
     // this function should take in the tab and update the state with the new tab.
+
+    this.setState({
+      selected: (tab)
+    })
   };
 
   filterCards = () => {
@@ -39,7 +44,16 @@ export default class Content extends Component {
           of the items from cardData. 
         - else, it should only return those cards whose 'tab' matched this.state.selected.
     */
-    return this.state.cards;
+  
+    const newCard = this.state.cards.filter(card => card.tab.includes(this.state.selected) )
+   
+
+    if(this.state.selected === 'all'){
+      return this.state.cards;
+    }else {
+      return newCard
+    }
+    
   };
 
   render() {
@@ -50,7 +64,7 @@ export default class Content extends Component {
           `selectedTab` that includes the currently selected tab
           and `selectTabHandler` that includes the function to change the selected tab
         */}
-        <Tabs tabs={this.state.tabs} />
+        <Tabs tabs={this.state.tabs}  selectedTab={this.state.selected} selectTabHandler={this.changeSelected}/>
         <Cards cards={this.filterCards()} />
       </div>
     );
